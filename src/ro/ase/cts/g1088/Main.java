e7e59f41198804b8eccd9a92a5c4f9603cfc5faf
negoitaariadna19 <negoitaariadna19@stud.ase.ro>
2022-03-03 09:21:32 +0200

    project created - hello world ok

--- src/ro/ase/cts/g1088/Main.java
+++ src/ro/ase/cts/g1088/Main.java
@@ -1,8 +1,23 @@
 package ro.ase.cts.g1088;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Introduceti propozitia");
+        String str=sc.nextLine();
+        try{
+            if(str.toLowerCase()==str)
+            {
+                throw new ExceptionAllower();
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.print("Ati introdus+" + str);
     }
 }
